5ecd2eb69b1183368d003e9e38c3ecde413939ca
chanhong1206 <chanhongy6@naver.com>
2023-03-17 02:04:53 +0000

    file input output

---
+++ filewr3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+
+void file1(char * fileName){
+	int fd = open(fileName, O_RDWR, 0644);
+	//int fd = open(fileName, O_RDWR);
+	//int fd = open(fielName, O_RDONLY | O_CREAT);
+	//int fd = open(fielName, O_WRONLY | O_CREAT, 0664);
+	//int fd = open(fielName, O_WRONLY | O_CREAT | O_EXCL, 0644);
+	if (fd < 0){
+		printf("%s(%d)\n", strerror(errno), errno);
+	}
+}
+
+void file2(char *fileName){
+	int fd = open(fileName, O_RDWR|O_CREAT, 0644);
+	char a[1024] = "01234567890123";
+	printf("%d\n", (int)write(fd, a, 10));
+	printf("%s(%d)\n", strerror(errno), errno);
+}
+
+int main(int argc, char **argv){
+	if (argc != 3) exit(0);
+	file1(argv[1]);
+	file2(argv[2]);
+}
