1d2ead17f0a0dd0301526773c6a5ada4e5bd6334
chanhong1206 <chanhongy6@naver.com>
2023-03-31 11:31:42 +0000

    commit

---
+++ ex05-05.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#define BUF_SIZE 256
+
+void printcwd(){
+    char buffer[BUF_SIZE];
+    if (getcwd(buffer, BUF_SIZE) == NULL) exit(1);
+
+    printf("%s\n", buffer);
+}
+
+int main(){
+    printcwd();
+    chdir("/usr/include");
+    printcwd();
+    chdir("..");
+}
