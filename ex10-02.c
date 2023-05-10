bdd8ff57dd07dbea8fd5339fa541d63c9c8d0208
yoochanhong <chanhongy6@naver.com>
2023-05-10 03:38:59 +0000

    commit

---
+++ ex10-02.c
@@ -0,0 +1,16 @@
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+
+
+int main() {
+    sigset_t set;
+    int result;
+
+    sigemptyset(&set);
+    result = sigismember(&set, SIGALRM);
+    printf("SIGALRM is %s a member\n", result ? "" : "not");
+    sigaddset(&set, SIGALRM);
+    result = sigismember(&set, SIGALRM);
+    printf("SIGALRM is %s a member\n", result ? "" : "not");
+}
